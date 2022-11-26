115ae83e6c9e477a26b5f2e5b29fb0eec64fa205
hahayz77 <danilo__danilo@hotmail.com>
2022-11-26 17:29:18 -0300

    Data from sanity OK

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import { client } from '../lib/client';
 const Home = ({products, bannerData}) => {
   return(
     <div>
-      <HeroBanner />
-      {console.log(products)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+      {/* {console.log(bannerData[0].buttonText)} */}
       <div className="products-heading">
         <h2>Index</h2>
         <p>some product informations</p>
